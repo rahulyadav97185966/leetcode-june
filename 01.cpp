e3ee87b1dca74d60dad7a69d2ea7f6864e456975
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-06-01 17:55:17 +0530

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,25 @@
+// Invert Binary Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        struct TreeNode *tmp;
+        if(root)
+        {
+            tmp = invertTree(root->left);
+            root->left=invertTree(root->right);
+            root->right =tmp;
+            
+        }
+        return root;
+    }
+    
+};
