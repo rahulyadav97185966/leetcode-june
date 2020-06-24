95ee415a8d22a031dd4615ddfb0dd44ca09e9899
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-06-24 20:35:22 +0530

    Create 24.cpp

---
+++ 24.cpp
@@ -0,0 +1,18 @@
+//Unique Binary Search Trees
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+
+        dp[1] = dp[0] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] +=  dp[i - 1 - j] * dp[j];
+            }
+        }
+        
+        return dp[n];
+    }
+};
